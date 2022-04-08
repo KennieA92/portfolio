f7d3bc5d5b82e75fded9d37458dd96dd663a7fba
KennieA92 <91551710+KennieA92@users.noreply.github.com>
2022-04-08 16:55:48 +0200

    Knockout Text

--- src/views/StrictlyPortfolioView.vue
+++ src/views/StrictlyPortfolioView.vue
@@ -1,5 +1,7 @@
 <template>
-  <div></div>
+  <div class="portfolio">
+    <div id="clip">KENNIE</div>
+  </div>
 </template>
 
 <script>
@@ -7,4 +9,33 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
+.portfolio {
+  background: linear-gradient(
+      to bottom,
+      lighten($primary-color, 5),
+      transparent
+    ),
+    url("@/assets/noise.svg");
+  #clip {
+    /*
+  Ensure background is added first
+  */
+    background: linear-gradient(
+        to bottom,
+        $primary-color,
+        rgba(222, 112, 6, 0.2),
+        $secondary-color
+      ),
+      url("@/assets/color.gif");
+    background-size: contain;
+
+    background-attachment: fixed;
+    -webkit-text-fill-color: transparent;
+    -webkit-background-clip: text;
+
+    font-size: 10vw;
+    font-weight: bold;
+    text-align: center;
+  }
+}
 </style>
